01a3a9a4fe5c5af802668218bbc8d04a2ec6eb06
5yndr0m <sdilanjana21@gmail.com>
2025-03-04 11:43:43 +0530

    feat: func for reading low level key presses

--- simple_text.c
+++ simple_text.c
@@ -38,6 +38,15 @@ void enableRawMode() {
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
+char editorReadKey() {
+  int nread;
+  char c;
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    if (nread == -1 && errno != EAGAIN) die("read");
+  }
+  return c;
+}
+
 /*** init ***/
 int main() {
   enableRawMode();
