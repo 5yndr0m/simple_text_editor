b6f606a4e8716b1a3a2d76305b3be76842d98e32
5yndr0m <sdilanjana21@gmail.com>
2025-03-04 18:07:19 +0530

    feat: get windows size hard way?

--- simple_text.c
+++ simple_text.c
@@ -12,6 +12,8 @@
 
 /*** data ***/
 struct editorConfig {
+  int screenrows;
+  int screencols;
   struct termios orig_termios; 
 };
 
@@ -56,10 +58,30 @@ char editorReadKey() {
   return c;
 }
 
+int getCursorPosition(int *rows, int *cols) {
+  char buf[32];
+  unsigned int i = 0;
+  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+  while (i < sizeof(buf) -1) {
+    if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+    if (buf[i] == 'R') break;
+    i++;
+  }
+  buf[i] = '\0';
+  
+  if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+  if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+  
+  return 0;
+}
+
 int getWindowSize(int *rows, int *cols) {
   struct winsize ws;
 
   if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+    return getCursorPosition(rows, cols);
     return -1;
   } else {
     *cols = ws.ws_col;
@@ -85,7 +107,7 @@ void editorProcessKeypress() {
 
 void editorDrawRows() {
   int y;
-  for (y = 0; y < 24; y++) {
+  for (y = 0; y < E.screenrows; y++) {
     write(STDOUT_FILENO, "~\r\n", 3);
   }
 }
@@ -100,8 +122,14 @@ void editorRefreshScreen() {
 }
 
 /*** init ***/
+
+void initEditor() {
+  if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+}
+
 int main() {
   enableRawMode();
+  initEditor();
 
   while (1) {
     char c = '\0';
