1fb00c35885ef88c4217275e89da0488455c4b7e
5yndr0m <sdilanjana21@gmail.com>
2025-04-09 21:54:39 +0530

    feat: limit right side scrolling

--- simple_text.c
+++ simple_text.c
@@ -218,6 +218,8 @@ void abFree(struct abuf *ab) {
 /*** input ***/
 
 void editorMoveCursor(int key) {
+    erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+    
   switch (key) {
     case ARROW_LEFT:
       if (E.cx != 0) {
@@ -225,7 +227,9 @@ void editorMoveCursor(int key) {
       }
       break;
     case ARROW_RIGHT:
+        if (row && E.cx < row->size){
         E.cx++;
+        }
       break;
     case ARROW_UP:
       if (E.cy != 0) {
